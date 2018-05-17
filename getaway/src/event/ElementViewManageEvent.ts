9c98eefb03fbc474418c6f639e57fdcec531aa0e
xiaoxiangxiao <15533339633@163.com>
2018-05-17 17:02:57 +0800

    自定义事件类

---
+++ getaway/src/event/ElementViewManageEvent.ts
@@ -0,0 +1,21 @@
+class ElementViewManageEvent extends egret.Event
+{	//ElementViewManageEvent类是自定义事件类
+	
+	//五种自定义事件类型
+	public static TAP_TWO_ELEMENT:string = "tap_two_element";
+	public static REMOVE_ANIMATION_OVER:string = "remove_animation_over";
+	public static UPDATE_MAP:string = "update_map";
+	public static UPDATE_VIEW_OVER:string = "update_view_over";
+
+	//属性
+	public propToElementLocation:number = 0;//携带道具点击元素的位置
+	public ele1:number = 0;//第一个点击的元素
+	public ele2:number = 0;//第二个点击的元素
+
+	public constructor(type:string, bubbles:boolean = false, cancelable:boolean = false)
+	{
+		super(type,bubbles,cancelable);
+	}
+
+
+}
